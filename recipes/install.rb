c40643eac52e11f0fb9f264c53d5ea0ce3c4e3e5
Jim Dowling <jdowling@sics.se>
2017-03-06 20:46:05 +0100

    allow different user/group combo

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,7 +1,13 @@
 # Nothing to do during install phase...
 
+group node.kkafka.group do
+  action :create
+  not_if "getent group #{node.kkafka.group}"
+end
+
 user node.kkafka.user do
   action :create
+  gid node.kkafka.group
   home "/home/#{node.kkafka.user}"
   shell "/bin/bash"
   manage_home true
