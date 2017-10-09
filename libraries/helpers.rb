40b251355c7d9c218e7a1ddfe3ecb9d0bea02702
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2017-10-09 17:45:30 +0200

    Fix log.dir and log.dirs location (#10)
    * fixing_log_dirs
    
    * typo

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -106,8 +106,9 @@ end
 def kafka_log_dirs
   dirs = []
   dirs += Array(node['kkafka']['broker']['log.dirs'])
-  dirs += Array(node['kkafka']['broker'].fetch(:log_dirs, []))
-  dirs += Array(node['kkafka']['broker'].fetch(:log, {}).fetch(:dirs, []))
+  dirs += Array(node['kkafka']['broker']['log.dir'])
+  #dirs += Array(node['kkafka']['broker'].fetch(:log_dirs, []))
+  #dirs += Array(node['kkafka']['broker'].fetch(:log, {}).fetch(:dirs, []))
   dirs.uniq!
   dirs
 end
